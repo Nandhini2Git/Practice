4937d9878c69d70a31d2cac66c7ffd80ccd2f99f
Generis <Generis@Nandhini>
2019-05-10 14:18:19 +0530

    hi uploading Practice

---
+++ src/Parallel.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+public class Parallel 
+{
+	public WebDriver driver;
+	@Parameters({"browser"})
+	@Test
+	public void testParallel(String browser)
+	{
+		System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+		System.setProperty("webdriver.ie.driver", "./Drivers/IEDriverServer.exe");
+		
+		if(browser.equals("firefox"))
+			driver=new FirefoxDriver();
+		else if(browser.equals("chrome"))
+			driver=new ChromeDriver();
+		else
+			driver=new InternetExplorerDriver();
+	}
+	
+}
